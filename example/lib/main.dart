e9f83f69dda082d1788fd3921737cd02cf4d37dc
Andre Loreth <andre@loreth.co>
2020-12-18 11:34:57 +0100

    chore: update example project

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -11,27 +11,92 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  MapKitSnapshotterBrightness brightness = MapKitSnapshotterBrightness.light;
+  int mapType = 0;
+  bool showsBuildings = false;
+  bool showsPointsOfIntereset = false;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('MapKit Snapshotter example'),
         ),
-        body: Center(
-          child: Image(
-            image: MapKitSnapshotterImageProvider(
-              MapKitSnapshotterOptions(
-                region: MapKitSnapshotterRegion(
-                  centerLatitude: 48.593946,
-                  centerLongitude: 12.208799,
-                  latitudeMeters: 1000,
-                  longitudeMeters: 1000,
+        body: Container(
+          width: double.infinity,
+          child: Column(
+            children: [
+              Image(
+                image: MapKitSnapshotterImageProvider(
+                  MapKitSnapshotterOptions(
+                    region: MapKitSnapshotterRegion(
+                      centerLatitude: 53.552363,
+                      centerLongitude: 9.990831,
+                      latitudeMeters: 1000,
+                      longitudeMeters: 1000,
+                    ),
+                    brightness: brightness,
+                    mapType: MapKitSnapshotterMapType.values[mapType],
+                    showsBuildings: showsBuildings,
+                    showsPointsOfInterest: showsPointsOfIntereset,
+                  ),
                 ),
+                width: 350,
+                height: 200,
               ),
-            ),
-            width: 350,
-            height: 200,
+              Column(
+                children: [
+                  Text("Brightness: ${brightness.toString()}"),
+                  Text("Map type: ${MapKitSnapshotterMapType.values[mapType]}"),
+                ],
+              ),
+              Column(
+                children: [
+                  MaterialButton(
+                    child: Text('Toogle brightness'),
+                    onPressed: () {
+                      setState(() {
+                        var brightness = MapKitSnapshotterBrightness.light;
+                        if (this.brightness ==
+                            MapKitSnapshotterBrightness.light)
+                          brightness = MapKitSnapshotterBrightness.dark;
+
+                        this.brightness = brightness;
+                      });
+                    },
+                  ),
+                  MaterialButton(
+                    child: Text('Switch MapType'),
+                    onPressed: () {
+                      setState(() {
+                        mapType++;
+                        if (mapType >= MapKitSnapshotterMapType.values.length)
+                          mapType = 0;
+                      });
+                    },
+                  ),
+                  MaterialButton(
+                    child:
+                        Text('${showsBuildings ? 'Hide' : 'Show'} buildings'),
+                    onPressed: () {
+                      setState(() {
+                        showsBuildings = !showsBuildings;
+                      });
+                    },
+                  ),
+                  MaterialButton(
+                    child: Text(
+                        '${showsPointsOfIntereset ? 'Hide' : 'Show'} points of intereset'),
+                    onPressed: () {
+                      setState(() {
+                        showsPointsOfIntereset = !showsPointsOfIntereset;
+                      });
+                    },
+                  ),
+                ],
+              )
+            ],
           ),
         ),
       ),
